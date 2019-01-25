529afcc88d1c32ee415eead9f45217aaeb12812b
Zhuang <zy22092@nottingham.edu.cn>
2019-01-24 23:55:52 -0500

    Add collider and raycast

--- COMP396_Project/Assets/Scripts/DrawObstacle.cs
+++ COMP396_Project/Assets/Scripts/DrawObstacle.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -7,14 +8,21 @@ public class DrawObstacle : MonoBehaviour
 {
     [SerializeField] private GameObject lineGeneratorPrefab;
     [SerializeField] private GameObject linePointPrefab;
-    [System.Serializable] private struct Obstacle
+    [System.Serializable] public struct Obstacle
     {
         public Vector3[] obstaclePoints;
     }
-    [SerializeField] private Obstacle[] obstacles = new Obstacle[5];
+    [SerializeField] private Obstacle[] obstacles;
     [SerializeField] private bool bUserDefine = false;
 
     private LinkedList<Vector3> cacheObstacle = new LinkedList<Vector3>();
+
+    // Getter and Setter
+    public Obstacle[] GetObstacles()
+    {
+        return obstacles;
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,29 +30,33 @@ public class DrawObstacle : MonoBehaviour
         if (!bUserDefine)
         {
             GenerateObstacles(obstacles);
+            GenerateCollider(obstacles);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (bUserDefine)
         {
-            Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            newPos.z = 0;
-            cacheObstacle.AddFirst(newPos);
-            CreatePointMarker(newPos);
-        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                newPos.z = 0;
+                cacheObstacle.AddFirst(newPos);
+                CreatePointMarker(newPos);
+            }
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            clearAllPoints();
-        }
+            if (Input.GetMouseButtonDown(1))
+            {
+                clearAllPoints();
+            }
 
-        if (Input.GetKeyDown("space"))
-        {
-            GenerateObstacle(new Obstacle { obstaclePoints = cacheObstacle.ToArray() });
-            cacheObstacle.Clear();
+            if (Input.GetKeyDown("space"))
+            {
+                GenerateObstacle(new Obstacle { obstaclePoints = cacheObstacle.ToArray() });
+                cacheObstacle.Clear();
+            }
         }
     }
 
@@ -89,7 +101,7 @@ public class DrawObstacle : MonoBehaviour
         lRend.SetPositions(linePoints);
     }
 
-    private void CreatePointMarker(Vector3 pointPosition)
+    private void CreatePointMarker(Vector2 pointPosition)
     {
         Instantiate(linePointPrefab, pointPosition, Quaternion.identity);
     }
@@ -101,6 +113,29 @@ public class DrawObstacle : MonoBehaviour
         foreach (GameObject p in allPoints)
         {
             Destroy(p);
+        }
+    }
+
+    private void GenerateCollider(int index, Obstacle obstacle)
+    {
+        PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
+        Vector2[] obstaclePoints2D = new Vector2[obstacle.obstaclePoints.Length];
+        for (int i = 0; i < obstacle.obstaclePoints.Length; i++)
+        {
+            obstaclePoints2D[i] = new Vector2(obstacle.obstaclePoints[i].x, obstacle.obstaclePoints[i].y);
+        }
+        polygonCollider.CreatePrimitive(obstacle.obstaclePoints.Length);
+        polygonCollider.SetPath(index, obstaclePoints2D);
+    }
+
+    private void GenerateCollider(Obstacle[] obstacles)
+    {
+        int index = 0;
+        foreach (Obstacle obstacle in obstacles)
+        {
+            // TODO fix the function
+            // GenerateCollider(index, obstacle);
+            index++;
         }
     }
 }
--- COMP396_Project/Assets/Scripts/ViewPoint.cs
+++ COMP396_Project/Assets/Scripts/ViewPoint.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class ViewPoint : MonoBehaviour
 {
-    // Cached
-    private GameObject viewpoint = GameObject.FindGameObjectWithTag("ViewPoint");
-    // private Vector3[] BoundaryLine =(DrawObstacle)GameObject.FindGameObjectWithTag("Boundary").GetBoundaryLine();
+    [SerializeField] private Vector3[] BoundaryLine;
+    [SerializeField] private GameObject IntersectionPointPrefab;
+    [SerializeField] private DrawObstacle.Obstacle[] ObstaclesLine;
 
     // Start is called before the first frame update
     void Start()
     {
+        DrawBoundary drawboundary = GameObject.Find("BoundaryManager").GetComponent<DrawBoundary>();
+        DrawObstacle drawobstacle = GameObject.Find("ObstacleManager").GetComponent<DrawObstacle>();
+
+        BoundaryLine = drawboundary.GetBoundaryLine();
+        ObstaclesLine = drawobstacle.GetObstacles();
+        GameObject viewpoint = GameObject.FindGameObjectWithTag("ViewPoint");
         if (viewpoint)
         {
+            if (BoundaryLine.Length != 0)
+            {
+                foreach (Vector3 boundaryPoint in BoundaryLine)
+                {
+                    Physics2D.Linecast(viewpoint.transform.position, boundaryPoint);
+                    Debug.DrawLine(viewpoint.transform.position, boundaryPoint, Color.blue, 100.0f);
+                }
 
-            // Physics2D.Linecast(viewpoint.transform.position, )
+                foreach (DrawObstacle.Obstacle obstacleLine in ObstaclesLine)
+                {
+                    foreach (Vector3 obstalePoint in obstacleLine.obstaclePoints)
+                    {
+                        RaycastHit2D rayCastHit2D = Physics2D.Linecast(viewpoint.transform.position, obstalePoint);
+                        Instantiate(IntersectionPointPrefab, rayCastHit2D.point, Quaternion.identity);
+                        Debug.DrawLine(viewpoint.transform.position, rayCastHit2D.point, Color.blue, 100.0f);
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("No element in BoundaryLine.");
+            }
         }
     }
 
